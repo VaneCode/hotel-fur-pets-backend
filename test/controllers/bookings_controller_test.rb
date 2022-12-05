cfee62da785bdb8fd8e642623d53627e6d04e20e
aron-helu <aronmilahela@gmail.com>
2022-12-05 21:48:07 +0300

    Fix linters error

--- test/controllers/bookings_controller_test.rb
+++ test/controllers/bookings_controller_test.rb
@@ -1,35 +1,37 @@
-require "test_helper"
+require 'test_helper'
 
 class BookingsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @booking = bookings(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get bookings_url, as: :json
     assert_response :success
   end
 
-  test "should create booking" do
-    assert_difference("Booking.count") do
-      post bookings_url, params: { booking: { animal_name: @booking.animal_name, animal_type: @booking.animal_type, checking_in: @booking.checking_in, checking_out: @booking.checking_out, hotel_id: @booking.hotel_id, user_id: @booking.user_id } }, as: :json
+  test 'should create booking' do
+    assert_difference('Booking.count') do
+      post bookings_url, params: { booking: { animal_name: @booking.animal_name, animal_type: @booking.animal_type, checking_in: @booking.checking_in, checking_out: @booking.checking_out, hotel_id: @booking.hotel_id, user_id: @booking.user_id } },
+                         as: :json
     end
 
     assert_response :created
   end
 
-  test "should show booking" do
+  test 'should show booking' do
     get booking_url(@booking), as: :json
     assert_response :success
   end
 
-  test "should update booking" do
-    patch booking_url(@booking), params: { booking: { animal_name: @booking.animal_name, animal_type: @booking.animal_type, checking_in: @booking.checking_in, checking_out: @booking.checking_out, hotel_id: @booking.hotel_id, user_id: @booking.user_id } }, as: :json
+  test 'should update booking' do
+    patch booking_url(@booking),
+          params: { booking: { animal_name: @booking.animal_name, animal_type: @booking.animal_type, checking_in: @booking.checking_in, checking_out: @booking.checking_out, hotel_id: @booking.hotel_id, user_id: @booking.user_id } }, as: :json
     assert_response :success
   end
 
-  test "should destroy booking" do
-    assert_difference("Booking.count", -1) do
+  test 'should destroy booking' do
+    assert_difference('Booking.count', -1) do
       delete booking_url(@booking), as: :json
     end
 
--- test/controllers/hotels_controller_test.rb
+++ test/controllers/hotels_controller_test.rb
@@ -1,35 +1,35 @@
-require "test_helper"
+require 'test_helper'
 
 class HotelsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @hotel = hotels(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get hotels_url, as: :json
     assert_response :success
   end
 
-  test "should create hotel" do
-    assert_difference("Hotel.count") do
+  test 'should create hotel' do
+    assert_difference('Hotel.count') do
       post hotels_url, params: { hotel: { description: @hotel.description, location: @hotel.location, name: @hotel.name, photo: @hotel.photo, price: @hotel.price, rating: @hotel.rating } }, as: :json
     end
 
     assert_response :created
   end
 
-  test "should show hotel" do
+  test 'should show hotel' do
     get hotel_url(@hotel), as: :json
     assert_response :success
   end
 
-  test "should update hotel" do
+  test 'should update hotel' do
     patch hotel_url(@hotel), params: { hotel: { description: @hotel.description, location: @hotel.location, name: @hotel.name, photo: @hotel.photo, price: @hotel.price, rating: @hotel.rating } }, as: :json
     assert_response :success
   end
 
-  test "should destroy hotel" do
-    assert_difference("Hotel.count", -1) do
+  test 'should destroy hotel' do
+    assert_difference('Hotel.count', -1) do
       delete hotel_url(@hotel), as: :json
     end
 
