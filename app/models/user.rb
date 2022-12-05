0ea9ccbcd4740e6b06ac81451ef75b79ba79fbc4
vanessaBonito <vanebonito@hotmail.com>
2022-12-05 15:45:37 -0500

    Add endpoints for hotel

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # Assosications
-  hhas_many :bookings
+  has_many :bookings
   has_many :hotels, through: :bookings
   # Validations
   validates :name, :phone, :email, presence: true
