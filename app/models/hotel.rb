b9ed5f6edebc9c24dcb8fb58c13a347ea2975849
vanessaBonito <vanebonito@hotmail.com>
2022-12-05 14:56:08 -0500

    Add validations

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,2 +1,8 @@
 class Hotel < ApplicationRecord
+  # Assosications
+  # Validations
+  validates :name, :location, :description, :photo, presence: true
+  validates :price, numericality: { only_float: true, greater_than: 0 }
+  validates :rating, numericality: { only_integer: true, greater_than: 0 }
+  # Methods
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  # Assosications
+  # Validations
+  validates :name, :phone, :email, presence: true
+  # Methods
 end
