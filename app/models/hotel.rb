fde35f535e28f966661ca61568ae2181fce32356
aron-helu <aronmilahela@gmail.com>
2022-12-05 23:04:09 +0300

    Add assosications for user, hotels and bookings

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,5 +1,7 @@
 class Hotel < ApplicationRecord
   # Assosications
+  has_many :bookings
+  has_many :users, through: :bookings
   # Validations
   validates :name, :location, :description, :photo, presence: true
   validates :price, numericality: { only_float: true, greater_than: 0 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   # Assosications
+  hhas_many :bookings
+  has_many :hotels, through: :bookings
   # Validations
   validates :name, :phone, :email, presence: true
   # Methods
