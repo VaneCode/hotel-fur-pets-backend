58ebf633c5daf89193dae2a138cfe72ba4dc1b9c
aron-helu <aronmilahela@gmail.com>
2022-12-16 16:23:55 +0300

    Fix user spec api doc

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -34,7 +34,6 @@ RSpec.describe 'users', type: :request do
   end
 
   path '/users/{id}' do
-    # You'll want to customize the parameter types...
     parameter name: 'id', in: :path, type: :string, description: 'id'
 
     get('show user') do
